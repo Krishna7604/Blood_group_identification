14386ed5829b8f5a7cc4726c270b3c904ffa3c04
Kathi Nagendra Babu <157376864+nagendrakathi@users.noreply.github.com>
2025-04-27 03:07:10 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from src import create_app, db
 from src.fingerprint.utils import load_blood_group_model
+import os
 
 app = create_app()
 
@@ -9,4 +10,4 @@ if __name__ == '__main__':
         # Load the model at startup
         if not load_blood_group_model():
             print("Warning: Failed to load the blood group prediction model!")
-    app.run(debug=True,port=5001)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
